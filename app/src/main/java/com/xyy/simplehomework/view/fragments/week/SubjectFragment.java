55d038be99526cd9640e011c465840823cfbeaa8
xyy <727160222@qq.com>
2018-02-27 19:21:18 +0800

    修复了在pxy手机上崩溃的bug

--- app/src/main/java/com/xyy/simplehomework/view/fragments/week/SubjectFragment.java
+++ app/src/main/java/com/xyy/simplehomework/view/fragments/week/SubjectFragment.java
@@ -69,7 +69,6 @@ public class SubjectFragment extends Fragment {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 getContext(),
                 R.layout.spinner_item_text,
-                R.id.textView,
                 getResources().getStringArray(R.array.week_subject_show_type));
         arrayAdapter.setDropDownViewResource(android.support.v7.appcompat.R.layout.support_simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);

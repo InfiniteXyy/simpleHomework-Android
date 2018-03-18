66d8eeab15799ba7ee510b32bb108447774c534b
xyy <727160222@qq.com>
2018-03-18 13:48:59 +0800

    增加了add的菜单，和add任务的视觉效果 下一步：重新设计week（包括选周和作业排布）

---
+++ app/src/main/java/com/xyy/simplehomework/view/fragments/home/DialogTask.java
@@ -0,0 +1,44 @@
+package com.xyy.simplehomework.view.fragments.home;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.xyy.simplehomework.R;
+
+import java.util.Arrays;
+
+/**
+ * Created by xyy on 2018/3/18.
+ */
+
+public class DialogTask extends DialogFragment {
+    public DialogTask() {
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_task_select, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_task, Arrays.asList("12", "34")) {
+            @Override
+            protected void convert(BaseViewHolder helper, String item) {
+            }
+        });
+        view.findViewById(R.id.close_btn).setOnClickListener((v)->dismiss());
+    }
+}
--- app/src/main/java/com/xyy/simplehomework/view/fragments/home/FragmentPlan.java
+++ app/src/main/java/com/xyy/simplehomework/view/fragments/home/FragmentPlan.java
@@ -5,12 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.xyy.simplehomework.R;
@@ -54,6 +57,28 @@ public class FragmentPlan extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new ExpandablePlanAdapter(sections);
+        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                PopupMenu popupMenu = new PopupMenu(getContext(), view);
+                popupMenu.getMenuInflater().inflate(R.menu.add_plan, popupMenu.getMenu());
+                popupMenu.show();
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.task:
+                                new DialogTask().show(getChildFragmentManager(), null);
+                                break;
+                            case R.id.homework:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
+
         recyclerView.setAdapter(adapter);
     }
 
@@ -114,6 +139,7 @@ public class FragmentPlan extends Fragment {
                             }
                         }
                     });
+                    helper.addOnClickListener(R.id.add);
                     break;
             }
         }
